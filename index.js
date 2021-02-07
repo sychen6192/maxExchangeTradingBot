0f3c1565d2b9ff156a8396c10b8cc16963a5d141
Jack SY Chen <jack_sy_chen@trendmicro.com>
2021-02-07 23:30:58 +0800

    [feat] second

--- index.js
+++ index.js
@@ -11,47 +11,50 @@ const max = new MAX({
 
 const rest = max.rest() // default version is 2
 
+async function main() {
+  while (true) {
+    await start();
+  }
+}
 
-start();
+main();
 
 async function start() {
-  // const supported = await rest.markets();
-  // console.log('Supported markets:', supported);
 
   const market = 'btcusdt';
+  const d = new Date(); 
 
-  // Step1 : Get current price of btcusdt
-  // console.log("Step1: Get current price of btcusdt")
-  // const tick = await rest.ticker({
-  //   market,
-  // });
-  // console.log(`The current best bid price of ${market} is ${tick.buy}`);
-  // console.log(`The current best ask price of ${market} is ${tick.sell}\n`);
-
-  // Step1 : Get current price of btcusdt from binance.
+  console.log(`Current time: ${d.toLocaleString()}`)
+  // 1) Get current price of btcusdt from binance.
+  console.log("1) Get current price of btcusdt from binance.");
   let binanceResponse = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
   let btcPriceBinance = binanceResponse.data.price;
   const tick = {
-    buy: (btcPriceBinance * 0.9).toString(),
-    sell: (btcPriceBinance * 1.1).toString()
+    buy: (btcPriceBinance * 0.99).toString(),
+    sell: (btcPriceBinance * 1.01).toString()
   }
 
-  // Step2: Check how much btc and usdt you have.
-  console.log("Step2: Check how much btc and usdt you have");
+  // 2) Check how much btc and usdt you have.
+  console.log("2) Check how much btc and usdt you have.");
   let btcAmount = await rest.account('btc');
   let usdtAmount = await rest.account('usdt');
-  console.log(`Current time: ${Date.now()}`)
-  console.log(`Current balance: btc=${btcAmount.balance} usdt=${usdtAmount.balance}\n`);
+  console.log(`Current balance: btc=${btcAmount.balance} usdt=${usdtAmount.balance}`);
 
-  // Step3: Set minimum order limitation for both, spread for 10%
-  console.log("Step3: Set minimum order limitation for both, spread for 10%")
+  // 3) Set minimum order limitation for both, spread for 1%
+  console.log("3) Set minimum order limitation for both, spread for 1%")
   const orderAmount = {
-    buy: '0.0005',
-    sell: '0.0005'
+    buy: '0.001',
+    sell: '0.001'
   }
 
-  if (usdtAmount.balance > 0) {
-    console.log(`Step3.1: Set buy order, Amount: ${orderAmount.buy} USDT`)
+  // 4) Cancel all orders, get ready for newer order
+  console.log("4) Cancel all orders, get ready for next order")
+  await rest.cancelOrders({
+    market: 'btcusdt'
+  })
+
+  if (usdtAmount.balance > orderAmount.buy) {
+    console.log(`4.1) Set buy order, Amount: ${orderAmount.buy} BTC`)
     const buyResponse = await rest.placeOrder({
       market,
       price: tick.buy,
@@ -59,12 +62,10 @@ async function start() {
       side: 'buy',
       ord_type: 'limit',
     });
-    // console.log(`The response of placing order:`, buyResponse);
-    // console.log("")
   }
 
-  if (btcAmount.balance > 0) {
-    console.log(`Step3.2: Set sell order, Amount: ${orderAmount.sell} BTC`)
+  if (btcAmount.balance > orderAmount.sell) {
+    console.log(`4.2) Set sell order, Amount: ${orderAmount.sell} BTC`)
     const sellResponse = await rest.placeOrder({
       market,
       price: `${tick.sell}`,
@@ -72,30 +73,15 @@ async function start() {
       side: sell,
       ord_type: 'limit',
     });
-    // console.log(`The response of placing order:`, sellResponse);
-    // console.log("")
   }
+  console.log("=============================================================================")
 
-  await sleep(1000);
+  await sleep(2000);
 
   // Step4: Get order history
-  console.log("Step4: Get order history")
-  const history = await rest.orders({ market, state: ['wait', 'convert', 'done'] });
+  // console.log("Step4: Get order history")
+  // const history = await rest.orders({ market, state: ['wait', 'convert', 'done'] });
   // console.log('Order history:', history)
-
-  // Step5: Cancel all orders, get ready for next order
-  console.log("Step5: Cancel all orders, get ready for next order")
-  await rest.cancelOrders({
-    market: 'btcusdt'
-  })
-
-
-  // Step6: If the order is executed successfully, you should see balance change.
-  console.log("Step6: If the order is executed successfully, you should see balance change.")
-  btcAmount = await rest.account('btc');
-  usdtAmount = await rest.account('usdt');
-  console.log(`Current time: ${Date.now()}`)
-  console.log(`Current balance: btc=${btcAmount.balance} usdt=${usdtAmount.balance}\n`);
 }
 
 function sleep(ms) {
